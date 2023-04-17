e41f44d83b09c9bf891fe31dbe576d349efa9359
a1a2a3a4a5 <linub7@gmail.com>
2023-04-17 17:46:55 +0430

    admin orders screen -> done

--- screens/admin/orders/index.js
+++ screens/admin/orders/index.js
@@ -1,13 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { colors, defaultStyle } from '../../../styles';
+import HeaderComponent from '../../../components/shared/header';
+import CommonAuthHeading from '../../../components/auth/heading';
+import CustomLoader from '../../../components/shared/custom-loader';
+import OrdersScreenOrderList from '../../../components/orders';
+
+const loading = false;
 
 const AdminOrdersScreen = () => {
   return (
-    <View>
-      <Text>AdminOrdersScreen</Text>
+    <View style={[defaultStyle, styles.container]}>
+      <HeaderComponent back={true} />
+      <View style={styles.innerContainer}>
+        <CommonAuthHeading heading={'Admin - All Orders'} />
+      </View>
+      {loading ? (
+        <View style={{ justifyContent: 'center', marginTop: 100 }}>
+          <CustomLoader size={100} color={colors.color3} />
+        </View>
+      ) : (
+        <OrdersScreenOrderList />
+      )}
     </View>
   );
 };
 
-export default AdminOrdersScreen;
+const styles = StyleSheet.create({
+  container: { backgroundColor: colors.color5 },
+  innerContainer: { paddingTop: 70 },
+});
 
-const styles = StyleSheet.create({});
+export default AdminOrdersScreen;
